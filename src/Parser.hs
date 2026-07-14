6d533889c05e7523b965c030f4610ec5530d8db4
Andrii Karach <andrii.karach.13@gmail.com>
2026-07-14 17:45:54 +0100

    Create some parsing functions in Parser.hs, implemented functor and applicative for Parser [Andrii Karach]

---
+++ src/Parser.hs
@@ -0,0 +1,50 @@
+module Parser where
+
+import Expr 
+import Eval 
+import Data.Char
+import Control.Applicative (many, some)
+
+arithmChar :: Op -> Char 
+arithmChar Add = "+"
+arithmChar Mul = '*'
+arithmChar Sub = '-'
+arithmChar Div = '/'
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String)}
+
+
+instance Functor Parser where 
+    -- fmap :: (a -> b) -> Parser a -> Parser b 
+    fmap f px = Parser $ \s ->
+        fmap (\(x, s') -> (f x, s')) (runParser px s) 
+
+
+instance Applicative Parser where 
+    -- pure :: a -> Parser a 
+    pure x = Parser $ \s -> Just (x, s)
+
+    -- <*> :: Parser (a -> b) -> Parser a -> Parser b 
+    pf <*> px = Parser $ \s -> do
+        (f, s')  <- runParser pf s 
+        (x, s'') <- runParser px s' 
+        return (f x, s'')
+        
+
+satisfy :: (Char -> Bool) -> Parser Char 
+satisfy p = Parser $ \s ->
+    case s of 
+        (c:cs) | p c -> Just (c, cs)
+        _            -> Nothing
+
+
+digit :: Parser Char 
+digit = satisfy isDigit 
+
+
+char :: Char -> Parser Char 
+char c = satisfy . (==)
+
+
+arithm :: Op -> Parser Char 
+arithm = char . arithmChar 
