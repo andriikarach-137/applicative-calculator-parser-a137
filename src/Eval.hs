4bf451987d77716cec283faaa36a3759dbdff4ce
Andrii Karach <andrii.karach.13@gmail.com>
2026-07-14 12:35:51 +0100

    Change eval function to return Maybe Double for safe division [Andrii Karach]

--- src/Eval.hs
+++ src/Eval.hs
@@ -1,15 +1,21 @@
 module Eval where
 
 import Expr 
+import Control.Applicative (liftA2)
 
-apply :: Op -> Double -> Double -> Double 
-apply Add = (+)
-apply Mul = (*)
-apply Sub = (-)
-apply Div = (/)
 
-eval :: Expr -> Double 
-eval (Val n) = n
-eval (BinOp Div _ 0) = undefined 
-eval (BinOp op e1 e2) = apply op $ op1 op2  
-eval _ = undefined 
+apply :: Op -> Double -> Double -> Maybe Double 
+apply Add = curry $ Just . uncurry (+)
+apply Mul = curry $ Just . uncurry (*)
+apply Sub = curry $ Just . uncurry (-)
+apply Div = safeDiv 
+
+
+safeDiv :: Double -> Double -> Maybe Double 
+safeDiv _ 0 = Nothing 
+safeDiv n m = Just $ n / m 
+
+
+eval :: Expr -> Maybe Double 
+eval (Val n) = Just n 
+eval (BinOp op e1 e2) = liftA2 (apply op) (eval e1) (eval e2)
